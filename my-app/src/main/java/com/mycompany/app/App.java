46109c61934f611dcd95aef181c7c14586617701
betrand1999 <132092468+Betrand1999@users.noreply.github.com>
2024-04-20 20:51:48 -0400

    build with maven

---
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,12 @@
+package com.mycompany.app;
+
+/**
+ * Welcome To Mr Betrand love, you have successfully built with Maven!
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Bertrand, you have successfully built with Maven!" );
+    }
+}
